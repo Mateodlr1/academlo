ca4cf2a78f46cd857167201e3b0318f4b4b2ddb1
Mateodlr1 <88116102+Mateodlr1@users.noreply.github.com>
2022-08-26 13:29:49 -0500

    Add files via upload

---
+++ ejercicios.js
@@ -0,0 +1,44 @@
+//primer ejercicio
+let Nombre = "Mateo";
+
+let Apeliddo = "Grisales";
+
+let Edad = 26;
+
+let Ciudad = "Manizales";
+
+
+
+//segundo ejercicio
+let Edades = [26, 14, 24];
+
+let Sum = Edades[0] + Edades[1] + Edades[2];
+
+console.log(Sum);
+
+//tercer ejercicio
+
+let user = {
+nombre: "Mateo",
+apellidos: "Grisales de la roche",
+edad: 26
+
+}
+let nombreCompleto = user.nombre + user.apellidos;
+
+console.log(nombreCompleto);
+
+//cuarto ejercicio
+let Persons = [
+{nombre: "Mateo", email: "mateodlr19@gmail.com", edad: 26},
+{nombre: "Andres", email: "andresdlr19@gmail.com", edad: 30},
+{nombre: "Sebastian", email: "sebastian19@gmail.com", edad: 40},
+{nombre: "Julian", email: "julian19@gmail.com", edad: 15},
+{nombre: "Camilo", email: "camilo19@gmail.com", edad: 16}
+
+
+];
+
+let correos = [Persons[0].email, Persons[1].email, Persons[2].email, Persons[3].email, Persons[4].email
+];
+console.log(correos);
